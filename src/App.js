10652e5fe087da223e71b2a562dd950affa85af0
Neil Padgen <neil.padgen@autodata-group.com>
2020-04-30 11:49:31 +0100

    Play a bell sound when the current event changes. You have to unmute the first time the page is loaded.  The button can't be muted again! HAHAHAHA

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { hot } from "react-hot-loader";
 import styled from "styled-components";
 import Clock from "react-live-clock";
-import { CurrentEvent } from "./current-event/CurrentEvent";
+import {
+  CurrentEvent,
+  BellSound,
+  renderSound,
+} from "./current-event/CurrentEvent";
 
 const AppContainer = styled.div`
   margin: 1rem;
@@ -15,8 +20,30 @@ const ClockContainer = styled.div`
   color: #aaaaaa;
 `;
 
+const MuteButton = styled.button`
+  font-size: 16px;
+  padding: 8px;
+  margin: 2px;
+  border: none;
+  border-radius: 8px;
+  outline: none;
+  cursor: pointer;
+  display: inline-block;
+  float: left;
+`;
+
+const unmute = () => {
+  document.getElementById("muteButton").innerHTML =
+    '<i class="fas fa-bell"></i>';
+  renderSound();
+};
+
 const App = () => (
   <AppContainer>
+    <div id="sound-container"></div>
+    <MuteButton id="muteButton" onClick={unmute}>
+      <i class="fas fa-bell-slash"></i>
+    </MuteButton>
     <ClockContainer>
       <Clock format={"h:mm:ss a"} ticking={true} />
     </ClockContainer>
--- src/current-event/CurrentEvent.js
+++ src/current-event/CurrentEvent.js
@@ -1,8 +1,10 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import styled from "styled-components";
 import ReactFitText from "react-fittext";
 import { getCurrentEvent, getNextEvent } from "./timetable";
 import moment from "moment";
+import Sound from "react-sound";
 
 const getEventBackgroundColour = (props) => {
   return props.event !== null && props.event.lesson ? "#88ff88" : "#ffff88";
@@ -15,11 +17,17 @@ const EventContainer = styled.div`
   position: relative;
   box-shadow: 0 4px 8px grey;
   text-align: center;
+  max-height: 18rem;
   background-color: ${(props) => getEventBackgroundColour(props)};
   display: ${(props) => (props.event == null ? "none" : "block")};
 `;
 
 const formatTime = (timeStr) => moment(timeStr, "HH:mm").format("h:mm");
+
+const htmlDecode = (input) => {
+  var doc = new DOMParser().parseFromString(input, "text/html");
+  return doc.documentElement.textContent;
+};
 
 const CurrentTimeDisplay = ({ event }) => {
   if (event == null) {
@@ -33,11 +41,18 @@ const CurrentTimeDisplay = ({ event }) => {
         </h1>
       </ReactFitText>
       <ReactFitText compressor={0.5}>
-        <h3>{event.text}</h3>
+        <h3>{htmlDecode(event.text)}</h3>
       </ReactFitText>
     </div>
   );
 };
+
+export const BellSound = () => (
+  <Sound
+    url="./class-bell-soundbible.com-1426436341.mp3"
+    playStatus={Sound.status.PLAYING}
+  />
+);
 
 const NextTimeDisplay = ({ event }) => {
   if (event == null) {
@@ -48,17 +63,29 @@ const NextTimeDisplay = ({ event }) => {
       <ReactFitText compressor={1.5}>
         <div>
           {formatTime(event.start)}&nbsp;&ndash;&nbsp;{formatTime(event.end)}:{" "}
-          {event.text}
+          {htmlDecode(event.text)}
         </div>
       </ReactFitText>
     </div>
   );
 };
 
+export const renderSound = () => {
+  console.log("Yay");
+  const soundContainer = document.getElementById("sound-container");
+  console.log(soundContainer);
+  if (soundContainer) {
+    ReactDOM.render(<BellSound />, soundContainer);
+  }
+};
+
 export class CurrentEvent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = {
+      date: new Date(),
+      currentEvent: {},
+    };
   }
 
   componentDidMount() {
@@ -79,8 +106,13 @@ export class CurrentEvent extends React.Component {
     const now = moment(this.state.date);
     const currentEvent = getCurrentEvent(now);
     const nextEvent = getNextEvent(now);
+    if (this.state.currentEvent !== currentEvent) {
+      this.setState({ currentEvent: currentEvent });
+      renderSound();
+    }
     return (
       <div>
+        <BellSound />
         <EventContainer event={currentEvent}>
           <CurrentTimeDisplay event={currentEvent} />
         </EventContainer>
